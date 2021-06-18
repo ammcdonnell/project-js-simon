8d5f34377c67233669b30bda49e150a5dc927404
elliebowers <ebowersche@brynmawr.edu>
2021-06-18 08:48:52 -0500

    added an s

--- site/simon.js
+++ site/simon.js
@@ -7,7 +7,7 @@ let humanSequence = [];
 const startButton = document.querySelector('.js-start');
 
 function startGame() {
-  startButton.classList.add('direction');
+  startButton.classList.add('directions');
 }
 
 startButton.addEventListener('click', startGame);
