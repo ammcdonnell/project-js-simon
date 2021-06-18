85285a7bfa6ae81e6ad6242e83194a146b79ff60
CQuiroga12 <77950094+CQuiroga12@users.noreply.github.com>
2021-06-18 15:50:12 -0400

    Added some game logic

--- site/buttons.js
+++ site/buttons.js
@@ -1,39 +1,54 @@
-
+var index = 0;
 // start button and directions
-const startButton = document.querySelector('.js-start');
-const info = document.querySelector('.js-info');
+const startButton = document.querySelector(".js-start");
+const info = document.querySelector(".js-info");
 
 function startGame() {
-  startButton.classList.add('hidden');
-  info.classList.remove('hidden');
-  info.textContent = 'Wait for the computer';
+  startButton.classList.add("hidden");
+  info.classList.remove("hidden");
+  info.textContent = "Wait for the computer";
+  //run the sequence
+  runGame(index);
+  //take the inputs
+  while(index != arraySize || !gameOver){}
+  //index = 0;
+  if(gameOver){
+    endGame();
+    return;
+  }
+  else{runGame}
+  //run the sequence
 }
-startButton.addEventListener('click', startGame);
+startButton.addEventListener("click", startGame);
 
 // light-up class - still working on it
-const div = document.createElement('div');
-div.className = 'light-up';
-document.querySelector('.light-up').style.opacity = '70%';
+// const div = document.createElement('div');
+// div.className = 'light-up';
+// document.querySelector('.light-up').style.opacity = '70%';
 
-let greenButton = document.querySelector('.simon-button.green');
-greenButton.addEventListener('click', function() {
-  alert('You clicked the green button');
-  div.classList.remove('light-up');
+let greenButton = document.querySelector(".simon-button.green");
+greenButton.addEventListener("click", function () {
+  // alert('You clicked the green button');
+  takeInput(0, index);
+  div.classList.remove("light-up");
 });
 
-let redButton = document.querySelector('.simon-button.red');
-redButton.addEventListener('click', function() {
-  alert('You clicked the red button');
+let redButton = document.querySelector(".simon-button.red");
+redButton.addEventListener("click", function () {
+  // alert("You clicked the red button");
+  takeInput(1, index);
 });
 
-let yellowButton = document.querySelector('.simon-button.yellow');
-yellowButton.addEventListener('click', function() {
-  alert('You clicked the yellow button');
+let yellowButton = document.querySelector(".simon-button.yellow");
+yellowButton.addEventListener("click", function () {
+  // alert("You clicked the yellow button");
+  takeInput(2, index);
 });
 
-let blueButton = document.querySelector('.simon-button.blue');
-blueButton.addEventListener('click', function() {
-  alert('You clicked the blue button');
+let blueButton = document.querySelector(".simon-button.blue");
+blueButton.addEventListener("click", function () {
+  // alert("You clicked the blue button");
+  takeInput(3, index);
 });
 
 // javascript classList
--- site/simon.js
+++ site/simon.js
@@ -3,7 +3,79 @@
 
 let sequence = [];
 let humanSequence = [];
-  let level = 0;
+let level = 0;
+
+let greenButton = document.querySelector(".simon-button.green");
+let redButton = document.querySelector(".simon-button.red");
+let yellowButton = document.querySelector(".simon-button.yellow");
+let blueButton = document.querySelector(".simon-button.blue");
 
 // comparison of what the computer has entered vs. what the user has entered
 // how to make the sequence of buttons
+var newNumber;
+var gameOver = false;
+
+//Adds a random integer between 0-3 that correspond to the 4 buttons that can be pressed
+function addtoSequence() {
+  newNumber = Math.random() * 4;
+  newNumber = Math.floor(newNumber);
+  sequence.push(newNumber);
+  humanSequence.push(newNumber);
+}
+
+function takeInput(buttonPressed, index) {
+  //Everytime you press a button the index of that press will be updated
+  //in the human sequence. This is called in button.js
+  humanSequence[index] = buttonPressed;
+  //After it is updated the button is sequences are checked
+  checkInput();
+  //After the sequences are checked, the index is updated so the next button can be pressed
+  index++;
+}
+
+function checkInput() {
+  //If you click the wrong button the two arrays will be different and it will be game over
+  if (sequence != humanSequence) {
+    gameOver = true;
+  }
+}
+
+function flash(simonButton) {
+  //Flash the button for a specified amount of time;
+}
+
+function runGame(index) {
+  //Adds a button to the sequence
+  addtoSequence();
+  //Flashes all the buttons in the sequence in order
+  for (button in sequence) {
+    if ((button = 0)) {
+      flash(greenButton);
+    } else if ((button = 1)) {
+      flash(redButton);
+    } else if ((button = 2)) {
+      flash(yellowButton);
+    } else if ((button = 3)) {
+      flash(blueButton);
+    }
+  }
+  //Does nothing until you have pressed all the buttons correctly
+  //or you press the wrong button and lose the game
+  while (index != arraySize || !gameOver) {}
+
+  //If game over end the game
+  if (gameOver) {
+    endGame();
+
+    //Else run the game function again
+  } else {
+    runGame;
+  }
+}
+
+function endGame() {
+  //Resets the sequences
+  sequence = [];
+  humanSequence = [];
+  //Show your score and have a play again button maybe?
+}
