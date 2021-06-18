b11af66f23becdbc0bde41a7b48aa3d763870b1d
elliebowers <ebowersche@brynmawr.edu>
2021-06-18 08:48:16 -0500

    Update simon.js

--- site/simon.js
+++ site/simon.js
@@ -1,2 +1,18 @@
 // This file contains the game logic.
 // All the event-listening should happen in buttons.js
+
+let sequence = [];
+let humanSequence = [];
+
+const startButton = document.querySelector('.js-start');
+
+function startGame() {
+  startButton.classList.add('direction');
+}
+
+startButton.addEventListener('click', startGame);
+
+
+
+
+
