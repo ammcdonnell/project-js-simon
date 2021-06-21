a8729425073f28ca7859ff9ee53cd1b14b6cadf6
ammcdonnell <amcdonnell@brynmawr.edu>
2021-06-21 12:08:11 -0400

    adding to the testing page

--- site/testingcode.js
+++ site/testingcode.js
@@ -8,49 +8,65 @@ const sequence = [
     blueButton
 ];
 
-
-
-const sequence = [
-  greenButton,
-    redButton,
-    yellowButton,
-    blueButton
-];
-
-const flash = (simon-button) => {
-  return new Promise((resolve, reject) => {
-    simon-button.className += ' active';
-    setTimeout(() => {
-      simon-button.className = simon-button.ClassName.replace(
-        ' active'
-        ''
-        );
-        setTimeout(() => {}
-        resolve();
-    }, 250);
-     1000);
-    });
-};
-
-let playerClick = false;
-const buttonClicked = simon-button => {
-  if (!playerClick) return;
-  console.log(panel);
-};
-
-const main = async () => {
-  for (let panel of sequence) {
-    await flash(simon-button);
-  }
-playerClick = true;
-};
+// getting a random button
 
 const getRandomButton = () => {
-  const simon-buttons = {
+  const simon-buttons = [
     greenButton,
     redButton,
     yellowButton,
     blueButton
   ];
-  return simon-buttons [parseInt(Math.random() * simon-buttons.length)];
+return simon-buttons[parseInt](Math.random() * simon-buttons.length)]
 };
+
+const sequence = [getRandomButton()];
+let humanSequence = [...sequence];
+
+// flash sequence
+
+const flash = simon-button => {
+  return new Promise((resolve) => {
+    simon-button.className += ' active';
+    setTimeout(() => {
+      simon-button.className = simon-button.ClassName.replace(
+        // adding light up class active
+        ' active'
+        // removing this class
+        ''
+        );
+        setTimeout(() => {
+        resolve();
+    }, 250);
+   }, 1000);
+ });
+};
+
+// checking if player clicked on the right panel
+let playerClick = false;
+
+const buttonClicked = buttonClicked => {
+  if (!playerClick) return;
+  const expectedButton = humanSequence.shift();
+  if (expectedButton) === buttonClicked) {
+    if (humanSequence.length === 0) {
+      // start new round
+      sequence.push(getRandomButton)());
+      humanSequence = [...sequence];
+      startFlashing();
+    }
+   } else {
+    //  end game
+    alert('game over');
+   }
+};
+
+const startFlashing = async () => {
+  playerClick = false;
+  for (const simon-button of sequence) {
+    await flash(simon-button);
+  }
+  playerClick = true;
+};
+
+startFlashing();
