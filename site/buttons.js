76697f47a621008e3066f697a4777c3961f240e3
ammcdonnell <amcdonnell@brynmawr.edu>
2021-06-21 23:55:08 -0400

    back to commenting out audio files

--- site/buttons.js
+++ site/buttons.js
@@ -1,9 +1,9 @@
 // audio files
 
-let green = new Audio("sound1.mp3");
-let red = new Audio("sound2.mp3");
-let yellow = new Audio("sound3.mp3");
-let blue = new Audio("sound4.mp3");
+// let green = new Audio("sound1.mp3");
+// let red = new Audio("sound2.mp3");
+// let yellow = new Audio("sound3.mp3");
+// let blue = new Audio("sound4.mp3");
 
 // start button and directions
 const startButton = document.querySelector(".js-start");
@@ -27,8 +27,8 @@ greenButton.addEventListener("click", async function () {
   flash(".simon-button.green");
   await sleep(100);
   clearColor();
-  let sound = green.cloneNode();
-  sound.play();
+  // let sound = green.cloneNode();
+  // sound.play();
 });
 
 let redButton = document.querySelector(".simon-button.red");
@@ -39,8 +39,8 @@ redButton.addEventListener("click", async function () {
   flash(".simon-button.red");
   await sleep(100);
   clearColor();
-  let sound = red.cloneNode();
-  sound.play();
+  // let sound = red.cloneNode();
+  // sound.play();
 });
 
 let yellowButton = document.querySelector(".simon-button.yellow");
@@ -51,8 +51,8 @@ yellowButton.addEventListener("click", async function () {
   flash(".simon-button.yellow");
   await sleep(100);
   clearColor();
-  let sound = yellow.cloneNode();
-  sound.play();
+  // let sound = yellow.cloneNode();
+  // sound.play();
 });
 
 let blueButton = document.querySelector(".simon-button.blue");
@@ -63,8 +63,8 @@ blueButton.addEventListener("click", async function () {
   flash(".simon-button.blue");
   await sleep(100);
   clearColor();
-  let sound = blue.cloneNode();
-  sound.play();
+  // let sound = blue.cloneNode();
+  // sound.play();
 });
 
 // javascript classList
